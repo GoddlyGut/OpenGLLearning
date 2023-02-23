b5660d0557fd200f7cc0dfea30cd8df4161ad998
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-02-22 20:35:43 -0500

    cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,7 @@ int main()
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         lightingShader.setMat4("view", view);
